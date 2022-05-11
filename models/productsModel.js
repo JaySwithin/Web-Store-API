e43b2b70a929a83bc77bd3451621ceedd5631424
Swithin Okyere <jayswithin@gmail.com>
2022-05-11 22:57:20 +0000

    Added data for product model

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
-  
+  productName: String,
+  productDescription: String,
+  productCategory: {
+    type: String,
+    enum : ['PHONES','COMPUTERS', 'ELECTRONICS', 'FURNITURE', 'CLOTHES'],
+  },
+  quantityAvailable: Number,
+  dateAdded: Date
 });
 
 const productModel = mongoose.model("Products", ProductSchema);
