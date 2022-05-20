0da7916b4cdee89d69ef3fe9299ca0af241d1c91
Swithin Okyere <jayswithin@gmail.com>
2022-05-20 23:58:34 +0000

    Route to get all products completed

--- routes/products.js
+++ routes/products.js
@@ -6,9 +6,23 @@ const productModel = require("./../models/productsModel");
 
 router.get('/', (req, res) => {
   try {
-    
+    productModel.find({})
+    .then((result) => {
+      res.json({
+        status: "SUCCESS",
+        message: "Products successfully fetched.",
+        data: result
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+      res.json({
+        status: "FAILED",
+        message: "Error occured fetching data."
+      })
+    })
   } catch (error) {
-    
+    console.log(error)
   }
 })
 
