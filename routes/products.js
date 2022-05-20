c325f8769c0f7b26bef7723abcb2cf9146c614d9
Swithin Okyere <jayswithin@gmail.com>
2022-05-20 23:59:02 +0000

    Added try-catch statement

--- routes/products.js
+++ routes/products.js
@@ -4,26 +4,27 @@ const router = express.Router();
 // IMPORT PRODUCT MODEL.
 const productModel = require("./../models/productsModel");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   try {
-    productModel.find({})
-    .then((result) => {
-      res.json({
-        status: "SUCCESS",
-        message: "Products successfully fetched.",
-        data: result
+    productModel
+      .find({})
+      .then((result) => {
+        res.json({
+          status: "SUCCESS",
+          message: "Products successfully fetched.",
+          data: result,
+        });
       })
-    })
-    .catch((error) => {
-      console.log(error)
-      res.json({
-        status: "FAILED",
-        message: "Error occured fetching data."
-      })
-    })
+      .catch((error) => {
+        console.log(error);
+        res.json({
+          status: "FAILED",
+          message: "Error occured fetching data.",
+        });
+      });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
