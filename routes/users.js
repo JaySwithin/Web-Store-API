6cf0da2e0045d7dd2ab25922bffb1c129588396f
Swithin Okyere <jayswithin@gmail.com>
2022-05-25 23:43:22 +0000

    Done with login route

--- routes/users.js
+++ routes/users.js
@@ -10,9 +10,7 @@ const { route } = require("./products");
 router.post("/register", (req, res) => {
   try {
     let { fullName, email, phoneNumber, password } = req.body;
-    if (
-      [fullName, email, phoneNumber, password].every(Boolean)
-    ) {
+    if ([fullName, email, phoneNumber, password].every(Boolean)) {
       userModel
         .find({ email })
         .then((result) => {
@@ -23,28 +21,31 @@ router.post("/register", (req, res) => {
             });
           } else {
             const saltRounds = 10;
-            bcrypt.hash(password, saltRounds).then((hashedPassword) => {
-              const newUser = new userModel({
-                fullName,
-                email,
-                phoneNumber,
-                password,
-                verified: false,
-              });
+            bcrypt
+              .hash(password, saltRounds)
+              .then((hashedPassword) => {
+                const newUser = new userModel({
+                  fullName,
+                  email,
+                  phoneNumber,
+                  password: hashedPassword,
+                  verified: false,
+                });
 
-              newUser.save().then((result) => {
+                newUser.save().then((result) => {
+                  res.json({
+                    status: "SUCCESS",
+                    message: "Registered successfully.",
+                  });
+                });
+              })
+              .catch((error) => {
                 res.json({
-                  status: "SUCCESS",
-                  message: "Registered successfully.",
+                  status: "FAILED",
+                  message:
+                    "Could not register account. Please try again in a few minutes.",
                 });
               });
-            })
-            .catch((error) => {
-              res.json({
-                status: "FAILED",
-                message: "Could not register account. Please try again in a few minutes."
-              });
-            })
           }
         })
         .catch((error) => {
@@ -58,6 +59,48 @@ router.post("/register", (req, res) => {
       res.json({
         status: "FAILED",
         message: "Please fill all input fields",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// ROUTE TO LOG IN A USER
+router.post("/login", (req, res) => {
+  try {
+    let { email, password } = req.body;
+    if ([email, password].every(Boolean)) {
+      email = email.trim();
+      password = password.trim();
+
+      userModel.find({ email }).then((result) => {
+        if (result.length) {
+          const hashedPassword = result[0].password;
+          bcrypt.compare(password, hashedPassword).then((result) => {
+            if (result) {
+              res.json({
+                status: "SUCCESS",
+                message: "Login successful",
+              });
+            } else {
+              res.json({
+                status: "FAILED",
+                message: "Password incorrect.",
+              });
+            }
+          });
+        } else {
+          res.json({
+            status: "FAILED",
+            message: "User account with provided email does not exist.",
+          });
+        }
+      });
+    } else {
+      res.json({
+        status: "FAILED",
+        message: "Input fields cannot be empty",
       });
     }
   } catch (error) {
